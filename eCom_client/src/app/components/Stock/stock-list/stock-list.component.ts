f973c9a073aa2e79556ce11e503fbb0947e5e94b
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-13 23:03:22 +0600

    stock list user interface html, and value reference included from component.ts and model.ts

--- eCom_client/src/app/components/Stock/stock-list/stock-list.component.ts
+++ eCom_client/src/app/components/Stock/stock-list/stock-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Stock } from 'src/app/Model/stock.model';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html',
   styleUrls: ['./stock-list.component.css']
 })
-export class StockListComponent {
+export class StockListComponent implements OnInit{
+  
+    stocks: Stock[] = [];
+    constructor() { }
+  
+    ngOnInit(): void {
+    }
 
 }
