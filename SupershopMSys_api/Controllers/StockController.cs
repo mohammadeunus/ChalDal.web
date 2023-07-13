924ae1521184166ae149bda52c8f1dc20bf30bb9
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-14 01:42:59 +0600

    error: created stock controller

---
+++ SupershopMSys_api/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SupershopMSys_api.Data;
+
+namespace SupershopMSys_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        readonly SuperShopApiDbContext _Context;
+        public StockController(SuperShopApiDbContext context) 
+        {
+            _Context = context;
+        }
+
+        public async Task<IActionResult> GetAllStock()
+        {
+            var stockFetch = _Context.stocks.ToListAsync();
+            return Ok(stockFetch);
+        }
+
+
+    }
+}
