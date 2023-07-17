347889957207e4001a4794de0c7c293974369b05
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-17 23:54:45 +0600

    added some component in admin and user

--- eCom_client/src/app/app-routing.module.ts
+++ eCom_client/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StockListComponent } from './components/stock/stock-list/stock-list.component';
+import { StockListComponent } from './components/admin/stock-list/stock-list.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { DashBoardComponent } from './components/admin/dash-board/dash-board.component';
 
 const routes: Routes = [
   {
-    path: 'stocks',
-    component: StockListComponent
-  }
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashBoardComponent },
+      { path: 'stock', component: StockListComponent },  
+      // Add more admin routes as needed
+    ],
+  },
+
+
+  
 ];
 
 @NgModule({
--- eCom_client/src/app/app.component.ts
+++ eCom_client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './Services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Chaldal.com';
+  isUserLoggedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    // Subscribe to the isAuthenticated$ observable to get the latest authentication status
+    /*this.authService.isAuthenticated$.subscribe((loggedIn) => {
+      this.isUserLoggedIn = loggedIn;
+    });*/
+  }
 }
