c8039be8095fddd1f9f9a94534594161167a99da
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-18 17:48:36 +0600

    added product client side ui

---
+++ eCom_client/src/app/Services/product.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/enivronment/environment';
+import { Product } from '../Model/product.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  baseApiUrl:string =environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { } //to be able to talk http api we need to inject httpclient
+    getallStocks(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseApiUrl + 'api/products');
+  }
+}
--- eCom_client/src/app/components/admin/product-list/product-list.component.ts
+++ eCom_client/src/app/components/admin/product-list/product-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Product } from 'src/app/Model/product.model';
+import { ProductService } from 'src/app/Services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+  products: Product[] = [];
+  constructor(private ProductService: ProductService) { }
 
+  ngOnInit(): void {
+    this.ProductService.getallStocks()
+      .subscribe({
+          next: (products) => {
+            this.products = products;
+          },
+          error: (err: any) => console.log(err)
+      })      
+  }
 }
+ 
