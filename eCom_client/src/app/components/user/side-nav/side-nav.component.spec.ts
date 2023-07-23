bc6fb92c94fc615043f50c9a35c677b4ad9979b4
mohammad eunus <100eunus@gmail.com>
2023-07-23 17:46:40 +0600

    added new navbar (#8)

--- eCom_client/src/app/components/user/side-nav/side-nav.component.spec.ts
+++ eCom_client/src/app/components/user/side-nav/side-nav.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { SideNavComponent } from './side-nav.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('SideNavComponent', () => {
+  let component: SideNavComponent;
+  let fixture: ComponentFixture<SideNavComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [SideNavComponent]
     });
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(SideNavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
