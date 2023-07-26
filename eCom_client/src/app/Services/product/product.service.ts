aec6c27e3e7d3b3edbb3c9349de1264e80b36c45
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-26 15:02:34 +0600

    added product cards components to show customer the products

--- eCom_client/src/app/Services/product/product.service.ts
+++ eCom_client/src/app/Services/product/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/enivronment/environment';
 import { Product } from '../../Model/product.model';
 import { Observable } from 'rxjs';
+import { UserProduct } from 'src/app/Model/user-product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,9 @@ export class ProductService {
 
   constructor(private http: HttpClient) { } //to be able to talk http api we need to inject httpclient
     GetAllProduct(): Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseApiUrl + 'api/product');
+        return this.http.get<Product[]>(this.baseApiUrl + 'api/product');
+    }
+    GetAllUserProduct(): Observable<UserProduct[]>{
+      return this.http.get<UserProduct[]>(this.baseApiUrl + 'api/product');
   }
 }
--- eCom_client/src/app/app-routing.module.ts
+++ eCom_client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AdminComponent } from './components/admin/admin.component';
 import { DashBoardComponent } from './components/admin/dash-board/dash-board.component';
 import { UserComponent } from './components/user/user.component';
 import { ProductListComponent } from './components/admin/product-list/product-list.component';
+import { HomeComponent } from './components/user/home/home.component';
 
  
 const routes: Routes = [
@@ -24,6 +25,7 @@ const routes: Routes = [
     component: UserComponent,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
       // Add more user routes as needed
     ],
   }
