124e7ab7f6a84106da1d0f39c60e62dfcdbeb69d
mohammad eunus <100eunus@gmail.com>
2023-07-28 02:32:14 +0600

    created repository for ADD-product and IFORMFILE to add image of product (#11)

--- eCom_api/Model/ProductModel.cs
+++ eCom_api/Model/ProductModel.cs
@@ -2,29 +2,30 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace eCom_api.Model
+namespace eCom_api.Model;
+
+public class ProductModel
 {
-    public class ProductModel
-    {
-        [Key]
-        public int Id { get; set; }
-        public int PopularityCount { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public decimal Vat { get; set; }
-        public string? Description { get; set; }
-        public string? ImageUrl { get; set; }
-        public DateTime? ExpirationDate { get; set; }
-        public DateTime? LastUpdated { get; set; }
-        public DateTime? CreatedDate { get; set; }
+    [Key]
+    public int Id { get; set; }
+    public int PopularityCount { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public decimal Vat { get; set; }
+    public string? Description { get; set; }
+    [NotMapped]
+    public IFormFile? imageFile { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime? ExpirationDate { get; set; }
+    public DateTime? LastUpdated { get; set; }
+    public DateTime? CreatedDate { get; set; }
 
-        [MaxLength(50)]
-        public string? CreatedBy { get; set; }
-        [MaxLength(50)]
-        public string? UpdatedBy { get; set; }
-        public int? CategoryId { get; set; }
-        [ForeignKey("CategoryId")]
-        public CategoryModel? Category { get; set; }
+    [MaxLength(50)]
+    public string? CreatedBy { get; set; }
+    [MaxLength(50)]
+    public string? UpdatedBy { get; set; }
+    public int? CategoryId { get; set; }
+    [ForeignKey("CategoryId")]
+    public CategoryModel? Category { get; set; }
 
-    }
 }
